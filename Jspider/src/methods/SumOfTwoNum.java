7bfda84dd270a3bf8706743417e7f85d66a934af
devendramali12 <dmali9198@gmail.com>
2025-08-26 21:35:45 +0530

    SumOfTwoNumMethod

---
+++ Jspider/src/methods/SumOfTwoNum.java
@@ -0,0 +1,16 @@
+package methods;
+
+public class SumOfTwoNum {
+	public static int add(int num1, int num2) {
+		int sum = num1 + num2;
+		System.out.println("The Sum Of Given Two Number is:- " + sum);
+		return sum;
+
+	}
+
+	public static void main(String[] args) {
+		add(50, 10);
+
+	}
+
+}
