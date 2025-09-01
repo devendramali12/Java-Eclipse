a8fd13d94b229b26b9c9bddb32c8297e67269b70
devendramali12 <dmali9198@gmail.com>
2025-09-01 21:58:40 +0530

    Array

---
+++ Jspider/src/box5/Array1.java
@@ -0,0 +1,17 @@
+package box5;
+
+public class Array1 {
+	public static void main(String[] args) {
+		int[] arr; // 1 --> Array Declaration
+
+		arr = new int[5]; // 2--> Array Creation
+
+		arr[0] = 10;
+		arr[1] = 25;
+		arr[2] = 63;
+		arr[3] = 85;
+		arr[4] = 75;
+
+	}
+
+}
