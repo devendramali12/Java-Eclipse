e161fe8f60e7f29c2d662ce04d8850770c9b48a6
devendramali12 <dmali9198@gmail.com>
2025-10-13 21:28:43 +0530

    init commit

--- Logical_Prg/src/level1/Q7.java
+++ Logical_Prg/src/level1/Q7.java
@@ -1,8 +1,29 @@
 package level1;
 
+import java.util.Scanner;
+
 // --> Take three numbers and print the largest
 public class Q7 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter 1st Number");
+		int a = sc.nextInt();
+
+		System.out.println("Enter 2nd Number");
+		int b = sc.nextInt();
+
+		System.out.println("Enter 3rd Number");
+		int c = sc.nextInt();
+
+		if (a >= b && a >= c) {
+			System.out.println(a + " is the greatest number");
+		} else if (b >= a && b >= c) {
+			System.out.println(b + " is the greatest number");
+		} else {
+			System.out.println(c + " is the greatest number");
+		}
+		sc.close();
 
 	}
 
